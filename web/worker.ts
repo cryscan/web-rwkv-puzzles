18e4b922195192d98cdb38c9c19b857f86e7381c
WangCe <halowang1991@gmail.com>
2024-12-12 13:58:32 +0800

    clean up

--- web/worker.ts
+++ web/worker.ts
@@ -1,54 +1,50 @@
-importScripts('common.js')
+importScripts("common.js")
 
-var _tokenizer = initTokenizer('../assets/puzzle15_vocab.json')
-var _session: undefined | Promise<wasm_bindgen.Session> = undefined
+var _tokenizer = initTokenizer("../assets/puzzle15_vocab.json");
+var _session: undefined | Promise<wasm_bindgen.Session> = undefined;
 
-this.addEventListener(
-  'message',
-  async function (e: MessageEvent<Uint8Array[] | String>) {
+this.addEventListener("message", async function (e: MessageEvent<Uint8Array[] | String>) {
     if (e.data instanceof Array) {
-      let blob = new Blob(e.data)
-      _session = initSession(blob)
-      return
+        let blob = new Blob(e.data);
+        _session = initSession(blob);
+        return;
     }
 
-    if ((await _session) === undefined) {
-      this.postMessage(null)
-      this.postMessage('Error: Model is not loaded.')
-      return
+    if (await _session === undefined) {
+        this.postMessage(null);
+        this.postMessage("Error: Model is not loaded.");
+        return;
     }
 
-    var tokenizer = await _tokenizer
-    var session = await _session!
-    var info = session.info()
-    var sampler = new SimpleSampler(info)
+    var tokenizer = await _tokenizer;
+    var session = await _session!;
+    var info = session.info();
+    var sampler = new SimpleSampler(info);
 
-    var input = e.data
-    console.log(input)
+    var input = e.data;
+    console.log(input);
 
-    var prompt = `<input>\n<board>\n${input}</board>\n</input>\n`
-    var state = new StateId()
+    var prompt = `<input>\n<board>\n${input}</board>\n</input>\n`;
+    var state = new StateId;
 
-    var encoder = new TextEncoder()
-    var decoder = new TextDecoder()
+    var encoder = new TextEncoder;
+    var decoder = new TextDecoder;
 
-    var tokens = tokenizer.encode(encoder.encode(prompt))
+    var tokens = tokenizer.encode(encoder.encode(prompt));
     var out = []
-    console.log(`prompt length: ${tokens.length}`)
-    console.log(prompt)
-    console.log(tokens)
+    console.log(`prompt length: ${tokens.length}`);
+    console.log(prompt);
+    console.log(tokens);
 
-    await this.navigator.locks.request('model', async (lock) => {
-      let p = pipeline(session, tokens, state, sampler, [59], 1000000)
+    await this.navigator.locks.request("model", async (lock) => {
+        let p = pipeline(session, tokens, state, sampler, [59], 1000000);
 
-      this.postMessage(null)
+        this.postMessage(null);
 
-      for await (let token of p) {
-        let word = decoder.decode(tokenizer.decode(new Uint16Array([token])))
-        out.push(token)
-        this.postMessage({ word, token })
-      }
-    })
-  },
-  false
-)
+        for await (let token of p) {
+            let word = decoder.decode(tokenizer.decode(new Uint16Array([token])));
+            out.push(token);
+            this.postMessage({ word, token })
+        }
+    });
+}, false);
