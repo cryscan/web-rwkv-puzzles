4ac5dbcece5fca8b472fe00c8b8284ae09353aad
cryscan <cryscan@umich.edu>
2024-12-15 12:33:49 +0800

    Update color scheme.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -90,28 +90,46 @@ const Info = () => {
       <div>Welcome to the Web-RWKV puzzle solver in browser!</div>
       <div>
         Check
+        {' '}
         <a href='https://github.com/cryscan/web-rwkv-puzzles' target='_blank'>
           the Github repo
         </a>
+        {' '}
         for more details about this demo.
       </div>
       <div>
         Note that this demo runs on WebGPU so make sure that your browser
-        support it before running (See{' '}
+        support it before running (See
+        {' '}
         <a href='https://webgpureport.org/' target='_blank'>
           WebGPU Report
+          <svg
+            width='12'
+            height='12'
+            viewBox='0 0 12 12'
+            style={{ marginLeft: '4px' }}
+          >
+            <path
+              fill='currentColor'
+              d='M2.5 2v2h5.293L1 10.793 2.207 12 9 5.207V10.5h2V2z'
+            />
+          </svg>
         </a>
         ).
       </div>
       <div>
-        Thanks to{' '}
+        Thanks to
+        {' '}
         <a href='https://github.com/josephrocca/rwkv-v4-web' target='_blank'>
           josephrocca
-        </a>{' '}
-        and{' '}
+        </a>
+        {' '}
+        and
+        {' '}
         <a href='https://github.com/HaloWang' target='_blank'>
           HaloWang
-        </a>{' '}
+        </a>
+        {' '}
         for the awesome in-browser implementation and the website (I am
         totally unfamiliar with web dev LoL).
       </div>
@@ -202,6 +220,7 @@ function generateSolvablePuzzle(): number[] {
 }
 
 function buildPrompt(board: number[]): string {
+  // prettier-ignore
   let map = [
     "0  ", "1  ", "2  ", "3  ",
     "4  ", "5  ", "6  ", "7  ",
@@ -214,7 +233,7 @@ function buildPrompt(board: number[]): string {
     prompt += map[board[i]]
     if (i % 4 == 3) prompt += '\n'
   }
-  return prompt;
+  return prompt
 }
 
 const Puzzle = () => {
@@ -236,8 +255,8 @@ const Puzzle = () => {
     <div className='puzzle'>
       <div className='puzzle_title'>15 Puzzle</div>
       <RunInfo />
-      <Grid />
       <Controls />
+      <Grid />
     </div>
   )
 }
@@ -342,8 +361,8 @@ const Controls = () => {
         {displayState == 'running'
           ? '🤔 Running...'
           : finished
-            ? '🎉 Finished'
-            : '🚀 Start'}
+          ? '🎉 Finished'
+          : '🚀 Start'}
       </button>
     </div>
   )
@@ -358,9 +377,9 @@ const Cell = (options: {
 
   const expectedLabel = rowIndex * 4 + columnIndex + 1
 
-  var color = 'rgba(128, 200, 255, 1)'
-  if (label == expectedLabel) color = 'rgba(128, 255, 100, 1)'
-  if (label == 0) color = 'rgba(128, 0, 0, 0)'
+  var color = 'rgba(197, 211, 232, 1)'
+  if (label == expectedLabel) color = 'rgba(208, 232, 197, 1)'
+  if (label == 0) color = 'rgba(224, 195, 195, 0)'
 
   return (
     <div
