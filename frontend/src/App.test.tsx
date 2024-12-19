a2876722056a541494a0f52f6b8dfd2fd2f11759
WangCe <halowang1991@gmail.com>
2024-12-19 10:42:38 +0800

    remove useless files

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import Puzzle from './pages/Puzzle'
-
-test('renders learn react link', () => {
-  render(<Puzzle />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
