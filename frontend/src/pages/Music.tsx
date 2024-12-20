07b0368caddf63c91c02e3cf9f25a9de61e34a37
Leo Lin <4258leo@gmail.com>
2024-12-20 15:36:57 +0800

    [Frontend] [Music] add music layout & keyboard

--- frontend/src/pages/Music.tsx
+++ frontend/src/pages/Music.tsx
@@ -1,8 +1,35 @@
-export default function Music() {
+import Keyboard from "../components/music/keyboard/keyboard";
+import Player from "../components/music/player/player";
+import Controller from "../components/music/controller/controller";
+// import Intro from "../components/music/intro/intro";
+
+
+type MusicSection = {
+    Component: React.ComponentType;
+    label: string;
+}
+
+export default function Music(): JSX.Element {
+
+    const MUSIC_SECTIONS: MusicSection[] = [
+        // { Component: Intro, label: 'introduction-section' },
+        { Component: Controller, label: 'controller-section' },
+        { Component: Player, label: 'player-section' },
+        { Component: Keyboard, label: 'keyboard-section' }
+        
+    ];
 
     return (
-        <div className='w-full h-full flex flex-col items-center justify-center text-3xl border p-2'>
-            music
+        <div className='w-full h-full flex flex-col gap-2 p-2'>
+            {MUSIC_SECTIONS.map(({ Component, label }) => (
+                <div
+                    key={label}
+                    className="w-full h-1/3 "
+                    aria-label={label}
+                >
+                    <Component />
+                </div>
+            ))}
         </div>
     )
 }
