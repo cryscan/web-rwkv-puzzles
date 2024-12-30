f6ebbcff25129d16c463cbfae0f68d1c95f2820d
Molly Sophia <mollysophia379@gmail.com>
2024-12-30 21:54:48 +0800

    Modify music prompts
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- frontend/src/components/music/assets/example-prompts.ts
+++ frontend/src/components/music/assets/example-prompts.ts
@@ -1,11 +1,7 @@
 export const prompts = [
   {
     name: 'Lost time is never found again',
-    prompt: `S:2
-B:25
-E:5
-B:26
-L:1/4
+    prompt: `L:1/4
 M:4/4
 K:D
 "D" A F F`
@@ -13,330 +9,210 @@ K:D
 
   {
     name: 'Simple Melody',
-    prompt: `S:2
-B:9
-E:5
-B:8
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:C
 "C" A2 B2`
   },
   {
     name: 'Irish Jig',
-    prompt: `S:2
-B:10
-E:6
-B:10
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:D
 "D" A2 B2`
   },
   {
     name: 'Soft Spring Rain',
-    prompt: `S:2
-B:9
-E:5
-B:9
-L:1/8
+    prompt: `L:1/8
 M:2/4
 K:Emin
 "Em" (Be) (d`
   },
   {
     name: 'Happy Waltz',
-    prompt: `S:2
-B:8
-E:6
-B:8
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:D
 "D" A2 A>F"Bm"`
   },
   {
     name: 'Flower Picking Elf',
-    prompt: `S:2
-B:10
-E:4
-B:8
-L:1/8
+    prompt: `L:1/8
 M:2/4
 K:none
 [K:C]"C" gg g>`
   },
   {
     name: 'Babbling Stream',
-    prompt: `S:2
-B:17
-E:6
-B:16
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:Bb
  B, |"Bb" B,D`
   },
   {
     name: 'Galloping Steed',
-    prompt: `S:2
-B:9
-E:8
-B:9
-L:1/4
+    prompt: `L:1/4
 M:2/2
 K:Ddor
  d |"Am" c A`
   },
   {
     name: 'Pink Girl Heart',
-    prompt: `S:2
-B:9
-E:5
-B:8
-L:1/8
+    prompt: `L:1/8
 M:3/4
 K:Bb
  F |"Bb" B>A"Gm"`
   },
   {
     name: 'Harmonious Relationship',
-    prompt: `S:2
-B:8
-E:5
-B:8
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:D
 "D" A3 B A`
   },
   {
     name: 'Mountain Valley',
-    prompt: `S:2
-B:8
-E:3
-B:8
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:G
 "G" G,A,B,`
   },
   {
     name: 'Partner in Crime',
-    prompt: `S:2
-B:16
-E:5
-B:17
-L:1/8
+    prompt: `L:1/8
 M:2/4
 K:A
 "A" a2{f} e2 |{d} c2`
   },
   {
     name: 'Tiptoe Dance',
-    prompt: `S:2
-B:9
-E:7
-B:9
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:A
  E |"A" A>G`
   },
   {
     name: 'Heroic Story',
-    prompt: `S:2
-B:8
-E:8
-B:8
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:Ab
 "Ab" c3 B A3`
   },
   {
     name: 'Breeze Blowing',
-    prompt: `S:2
-B:9
-E:4
-B:9
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:Ab
  C>D |"Ab" E2`
   },
   {
     name: 'Celebration Preparation',
-    prompt: `S:2
-B:16
-E:4
-B:16
-L:1/8
+    prompt: `L:1/8
 M:2/4
 K:F
 "F" cc"C7" B`
   },
   {
     name: 'Hidden Crisis',
-    prompt: `S:2
-B:9
-E:4
-B:12
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:Ador
 "Am" a3 g e`
   },
   {
     name: 'A calm daily routine',
-    prompt: `S:2
-B:9
-E:5
-B:8
-L:1/8
+    prompt: `L:1/8
 M:3/4
 K:Db
  DF |"Db" A3`
   },
   {
     name: 'Sudden Shower',
-    prompt: `S:2
-B:9
-E:8
-B:10
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:D
  A/F/ |"D" D`
   },
   {
     name: 'Age Of Peace',
-    prompt: `S:2
-B:9
-E:8
-B:9
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:G
  GA |"G" B2`
   },
   {
     name: 'Recalling the past',
-    prompt: `S:2
-B:17
-E:5
-B:16
-L:1/8
+    prompt: `L:1/8
 M:3/4
 K:none
  ec |"Am" A4`
   },
   {
     name: 'I want to tell you',
-    prompt: `S:2
-B:9
-E:8
-B:9
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:A
  A/B/ |"A" c2`
   },
   {
     name: 'Plotting',
-    prompt: `S:2
-B:8
-E:6
-B:8
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:Ador
 "Am" eAA`
   },
   {
     name: 'When the clouds part',
-    prompt: `S:2
-B:9
-E:6
-B:9
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:Amin
  e |"Am" c2 A`
   },
   {
     name: 'The Way Home',
-    prompt: `S:2
-B:17
-E:4
-B:10
-L:1/8
+    prompt: `L:1/8
 M:3/4
 K:C
  G2 |"C" G6 | G2`
   },
   {
     name: 'Harvest Season',
-    prompt: `S:2
-B:16
-E:6
-B:16
-L:1/4
+    prompt: `L:1/4
 M:3/4
 K:G
 "G" D3/2 E/ D`
   },
   {
     name: "Open one's own heart wide",
-    prompt: `S:2
-B:9
-E:4
-B:8
-L:1/8
+    prompt: `L:1/8
 M:12/8
 K:Bb
  F |"Bb" d3 d3`
   },
   {
     name: 'Silent Protection',
-    prompt: `S:2
-B:9
-E:5
-B:10
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:G
  D |"G" G2 G`
   },
   {
     name: 'Joyful Mood',
-    prompt: `S:2
-B:9
-E:5
-B:9
-L:1/8
+    prompt: `L:1/8
 M:6/8
 K:F
  c |"F" FA`
   },
   {
     name: 'Visit the maternal family',
-    prompt: `S:2
-B:8
-E:6
-B:8
-L:1/8
+    prompt: `L:1/8
 M:2/2
 K:D
 "D" f2 A2 f2`
   },
   {
     name: 'Get away from it all',
-    prompt: `S:2
-B:1
-E:0
-B:29
-L:1/8
+    prompt: `L:1/8
 M:4/4
 K:G
  D GB |:"G"`
