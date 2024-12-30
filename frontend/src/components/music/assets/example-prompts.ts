edf477472714766d91625d9f92ee170779b3a899
Molly Sophia <mollysophia379@gmail.com>
2024-12-30 22:11:18 +0800

    Update prompt names
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- frontend/src/components/music/assets/example-prompts.ts
+++ frontend/src/components/music/assets/example-prompts.ts
@@ -1,6 +1,6 @@
 export const prompts = [
   {
-    name: 'Lost time is never found again',
+    name: 'Prompt #1',
     prompt: `L:1/4
 M:4/4
 K:D
@@ -8,210 +8,210 @@ K:D
   },
 
   {
-    name: 'Simple Melody',
+    name: 'Prompt #2',
     prompt: `L:1/8
 M:4/4
 K:C
 "C" A2 B2`
   },
   {
-    name: 'Irish Jig',
+    name: 'Prompt #3',
     prompt: `L:1/8
 M:6/8
 K:D
 "D" A2 B2`
   },
   {
-    name: 'Soft Spring Rain',
+    name: 'Prompt #4',
     prompt: `L:1/8
 M:2/4
 K:Emin
 "Em" (Be) (d`
   },
   {
-    name: 'Happy Waltz',
+    name: 'Prompt #5',
     prompt: `L:1/8
 M:4/4
 K:D
 "D" A2 A>F"Bm"`
   },
   {
-    name: 'Flower Picking Elf',
+    name: 'Prompt #6',
     prompt: `L:1/8
 M:2/4
 K:none
 [K:C]"C" gg g>`
   },
   {
-    name: 'Babbling Stream',
+    name: 'Prompt #7',
     prompt: `L:1/8
 M:6/8
 K:Bb
  B, |"Bb" B,D`
   },
   {
-    name: 'Galloping Steed',
+    name: 'Prompt #8',
     prompt: `L:1/4
 M:2/2
 K:Ddor
  d |"Am" c A`
   },
   {
-    name: 'Pink Girl Heart',
+    name: 'Prompt #9',
     prompt: `L:1/8
 M:3/4
 K:Bb
  F |"Bb" B>A"Gm"`
   },
   {
-    name: 'Harmonious Relationship',
+    name: 'Prompt #10',
     prompt: `L:1/8
 M:4/4
 K:D
 "D" A3 B A`
   },
   {
-    name: 'Mountain Valley',
+    name: 'Prompt #11',
     prompt: `L:1/8
 M:4/4
 K:G
 "G" G,A,B,`
   },
   {
-    name: 'Partner in Crime',
+    name: 'Prompt #12',
     prompt: `L:1/8
 M:2/4
 K:A
 "A" a2{f} e2 |{d} c2`
   },
   {
-    name: 'Tiptoe Dance',
+    name: 'Prompt #13',
     prompt: `L:1/8
 M:6/8
 K:A
  E |"A" A>G`
   },
   {
-    name: 'Heroic Story',
+    name: 'Prompt #14',
     prompt: `L:1/8
 M:4/4
 K:Ab
 "Ab" c3 B A3`
   },
   {
-    name: 'Breeze Blowing',
+    name: 'Prompt #15',
     prompt: `L:1/8
 M:4/4
 K:Ab
  C>D |"Ab" E2`
   },
   {
-    name: 'Celebration Preparation',
+    name: 'Prompt #16',
     prompt: `L:1/8
 M:2/4
 K:F
 "F" cc"C7" B`
   },
   {
-    name: 'Hidden Crisis',
+    name: 'Prompt #17',
     prompt: `L:1/8
 M:4/4
 K:Ador
 "Am" a3 g e`
   },
   {
-    name: 'A calm daily routine',
+    name: 'Prompt #18',
     prompt: `L:1/8
 M:3/4
 K:Db
  DF |"Db" A3`
   },
   {
-    name: 'Sudden Shower',
+    name: 'Prompt #19',
     prompt: `L:1/8
 M:6/8
 K:D
  A/F/ |"D" D`
   },
   {
-    name: 'Age Of Peace',
+    name: 'Prompt #20',
     prompt: `L:1/8
 M:4/4
 K:G
  GA |"G" B2`
   },
   {
-    name: 'Recalling the past',
+    name: 'Prompt #21',
     prompt: `L:1/8
 M:3/4
 K:none
  ec |"Am" A4`
   },
   {
-    name: 'I want to tell you',
+    name: 'Prompt #22',
     prompt: `L:1/8
 M:6/8
 K:A
  A/B/ |"A" c2`
   },
   {
-    name: 'Plotting',
+    name: 'Prompt #23',
     prompt: `L:1/8
 M:6/8
 K:Ador
 "Am" eAA`
   },
   {
-    name: 'When the clouds part',
+    name: 'Prompt #24',
     prompt: `L:1/8
 M:6/8
 K:Amin
  e |"Am" c2 A`
   },
   {
-    name: 'The Way Home',
+    name: 'Prompt #25',
     prompt: `L:1/8
 M:3/4
 K:C
  G2 |"C" G6 | G2`
   },
   {
-    name: 'Harvest Season',
+    name: 'Prompt #26',
     prompt: `L:1/4
 M:3/4
 K:G
 "G" D3/2 E/ D`
   },
   {
-    name: "Open one's own heart wide",
+    name: 'Prompt #27',
     prompt: `L:1/8
 M:12/8
 K:Bb
  F |"Bb" d3 d3`
   },
   {
-    name: 'Silent Protection',
+    name: 'Prompt #28',
     prompt: `L:1/8
 M:6/8
 K:G
  D |"G" G2 G`
   },
   {
-    name: 'Joyful Mood',
+    name: 'Prompt #29',
     prompt: `L:1/8
 M:6/8
 K:F
  c |"F" FA`
   },
   {
-    name: 'Visit the maternal family',
+    name: 'Prompt #30',
     prompt: `L:1/8
 M:2/2
 K:D
 "D" f2 A2 f2`
   },
   {
-    name: 'Get away from it all',
+    name: 'Prompt #31',
     prompt: `L:1/8
 M:4/4
 K:G
